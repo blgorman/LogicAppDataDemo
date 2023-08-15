f83390d04bcd1e75fc2123e3acaa3e8faad43036
Brian L. Gorman <blgorman@gmail.com>
2023-08-14 23:28:16 -0500

    updates and added default web

---
+++ SimpleProductDataViewer/SimpleProductDataViewer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleProductDataViewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
