1bdadf07294f20d66ae1cbb2a054416aaaf49672
Brian L. Gorman <blgorman@gmail.com>
2023-08-14 23:52:35 -0500

    Added quick reset

---
+++ SimpleProductDataViewer/SimpleProductDataViewer/Controllers/ResetController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleProductData;
+
+namespace SimpleProductDataViewer.Controllers
+{
+    public class ResetController : Controller
+    {
+        private readonly SimpleProductDataDbContext _context;
+
+        public ResetController(SimpleProductDataDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //clear out the products and new products tables for new run
+            _context.Products.RemoveRange(_context.Products);
+            _context.NewProducts.RemoveRange(_context.NewProducts);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
